56f4eb467a750ae5b36d6cbf5dcd2da5843bf35a
Yannick Fricke <yannickfricke@googlemail.com>
2019-07-08 01:29:02 +0200

    Added index file for the functions

---
+++ functions/index.sh
@@ -0,0 +1,11 @@
+# Load all SQLite functions
+source ./functions/sqlite.sh
+
+# Load all custom filesystem functions
+source ./functions/filesystem.sh
+
+# Load all migration functions
+source ./functions/migrations.sh
+
+# Load the ShowVersion function
+source ./functions/version.sh
