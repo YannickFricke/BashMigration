77213cb71813fce0e5ac4487960486475c160a71
Yannick Fricke <yannickfricke@googlemail.com>
2019-07-13 03:44:09 +0200

    Added the logging constants

--- constants/index.sh
+++ constants/index.sh
@@ -1,4 +1,5 @@
 source "$INCLUDE_PATH/constants/colors.sh"
+source "$INCLUDE_PATH/constants/logging.sh"
 source "$INCLUDE_PATH/constants/filesystem.sh"
 source "$INCLUDE_PATH/constants/sql.sh"
 source "$INCLUDE_PATH/constants/version.sh"
